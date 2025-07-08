5dd13399f6f1513014214ab4a656ae72ea9e4c98
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2025-07-08 12:23:34 -0500

    Change color to use color palette

--- ybt_website/src/components/TitleBanner.jsx
+++ ybt_website/src/components/TitleBanner.jsx
@@ -1,6 +1,6 @@
 function TitleBanner({ children }) {
   return (
-    <div className="flex w-screen justify-center bg-amber-200 py-4 font-bold text-2xl">
+    <div className="flex w-screen justify-center bg-soft-saffron py-4 font-bold text-2xl">
       {children}
     </div>
   );
