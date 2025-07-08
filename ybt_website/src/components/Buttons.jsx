ababc550bd088c48db24e8e0c74bab7fdce679b5
Safiya George <smg_dev@outlook.com>
2025-07-08 13:51:57 -0400

    Created button components to take props from source page so that text and redirection action is accurate while keeping same styles.

---
+++ ybt_website/src/components/Buttons.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+function Buttons ({to, children}){
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate(to);
+    };
+    return (
+        <button onClick={handleClick} className="justify-center font-bold text-sm bg-[#F5BE29] px-5 py-2 m-3 rounded shadow">
+        {children}
+        </button>
+    );
+}
+export default Buttons;
