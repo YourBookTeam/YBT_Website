b05ac3b6f5543d2cf8bb5aa514864d7b72315957
Safiya George <smg_dev@outlook.com>
2025-07-10 13:11:08 -0400

    Added note about function use.

--- ybt_website/src/components/Buttons.jsx
+++ ybt_website/src/components/Buttons.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+//This function is used to create a button that navigates to a different route when clicked.
 function Buttons ({to, children, className}) {
     const navigate = useNavigate();
 
