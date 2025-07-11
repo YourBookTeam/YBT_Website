9ce98a195b43adef9153deaf18eb95279b280773
Nathan Samuel <nathan413591@gmail.com>
2025-07-11 11:47:30 -0500

    Merge pull request #1 from YourBookTeam/feature/title-banner Dropdowns and read more

--- ybt_website/src/pages/Faq.jsx
+++ ybt_website/src/pages/Faq.jsx
@@ -1,18 +1,19 @@
 import { Link } from 'react-router-dom';
+import Dropdown1 from '../components/Dropdown1'; 
 
 function Faq() {
-
   return (
-    <div>
-      <div>FAQ Page</div>
+    <div className="p-10 bg-gray-50 min-h-screen font-montserrat">
+      <Dropdown1
+        question="What is book marketing?"
+        answer="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at lorem ac nisi bibendum sodales. Praesent congue velit nec orci feugiat, et convallis metus fermentum."
+      />
 
-      <div>
-        <Link to="/">Go To Home</Link>
-      </div>   
-
+      <div className="mt-8">
+        <Link to="/" className="hover:text-black-800">Go To Home</Link>
+      </div>
     </div>
   );
-
 }
 
 export default Faq;
--- ybt_website/src/pages/Services.jsx
+++ ybt_website/src/pages/Services.jsx
@@ -1,18 +1,22 @@
 import { Link } from 'react-router-dom';
+import ReadMore from '../components/ReadMore'; 
 
 function Services() {
+  const longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, velit ac ullamcorper gravida, erat risus fermentum lacus, eget dapibus erat turpis non sapien. Nullam placerat sapien ut nisi euismod, nec vehicula urna porta. In vel sapien eget magna eleifend commodo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed facilisis, nisi non laoreet fringilla, purus lacus eleifend metus, nec rhoncus sem nulla in ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, velit ac ullamcorper gravida, erat risus fermentum lacus, eget dapibus erat turpis non sapien. Nullam placerat sapien ut nisi euismod, nec vehicula urna porta. In vel sapien eget magna eleifend commodo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed facilisis, nisi non laoreet fringilla, purus lacus eleifend metus, nec rhoncus sem nulla in ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, velit ac ullamcorper gravida, erat risus fermentum lacus, eget dapibus erat turpis non sapien. Nullam placerat sapien ut nisi euismod, nec vehicula urna porta. In vel sapien eget magna eleifend commodo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed facilisis.`;
 
   return (
-    <div>
-      <div>Services Page</div>
+    <div className="flex flex-col items-center py-10">
+      <h1 className="text-3xl font-bold mb-6">Services</h1>
 
-      <div>
-        <Link to="/">Go To Home</Link>
-      </div>   
+      <ReadMore text={longText} />
 
+      <div className="mt-6">
+        <Link to="/" className="hover:text-black-800">
+          Home
+        </Link>
+      </div>
     </div>
   );
-
 }
 
 export default Services;
