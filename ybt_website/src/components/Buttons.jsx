bfb620976fa69592e96203563d9aaa29e9578ee6
Safiya-Yourbookteam-website-intern <safiyayourbookteam@gmail.com>
2025-07-11 12:53:07 -0400

    removed hard-coded style

--- ybt_website/src/components/Buttons.jsx
+++ ybt_website/src/components/Buttons.jsx
@@ -7,7 +7,7 @@ function Buttons ({to, children, className}){
         navigate(to);
     };
     return (
-        <button onClick={handleClick} className={"justify-center font-bold text-sm bg-[#F5BE29] px-5 py-2 m-3 rounded shadow"}>
+        <button onClick={handleClick} className={className}>
         {children}
         </button>
     );
