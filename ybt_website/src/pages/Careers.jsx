d0c8fc42e234c51f2a22c73a22a3bc92b88d77d4
britneyhu <britneyhu2@gmail.com>
2025-07-14 10:17:19 -0700

    added dummy search bar

--- ybt_website/src/pages/Careers.jsx
+++ ybt_website/src/pages/Careers.jsx
@@ -1,10 +1,23 @@
 import { Link } from 'react-router-dom';
+import { useState } from "react";
 import TitleBanner from "../components/TitleBanner";
 import headshot_intern_rachel from "../assets/headshot_intern_rachel.png";
 import headshot_intern_nadun from "../assets/headshot_intern_nadun.png";
 import headshot_intern_genevieve from "../assets/headshot_intern_genevieve.png";
 
-function Careers() {
+function Careers({ onSearch }) {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    // onSearch(query);
+    console.log(query);
+    setQuery("");
+  }
+
+  const handleChange = (event) =>{
+    setQuery(event.target.value);
+  }
 
   return (
     <div> 
@@ -67,7 +80,16 @@ function Careers() {
           <div>
           </div>
         </div>
+      </div>
 
+      <div className="flex flex-col justify-center items-center gap-4">
+        <div className="font-bold text-[20px]">Current Openings</div>
+        <form onSubmit={handleSubmit} className="flex gap-2 text-[15px]">
+          <input type="text" value={query} onChange={handleChange} placeholder="Search by keyword..." 
+          className="w-150 h-8 px-2 py-1 border border-gray-300 rounded-md focus:outline-none bg-light-gray"/>
+
+          <button type="submit" className="w-20 h-8 px-4 py-2 bg-saffron rounded-md text-center flex flex-col items-center justify-center cursor-pointer">Search</button>
+        </form>
       </div>
 
     </div>
