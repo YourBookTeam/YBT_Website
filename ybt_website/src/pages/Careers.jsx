7f14b2d48143411d8fcba58b29888f45f285f563
britneyhu <britneyhu2@gmail.com>
2025-07-14 09:19:17 -0700

    added banner and top title

--- ybt_website/src/pages/Careers.jsx
+++ ybt_website/src/pages/Careers.jsx
@@ -1,14 +1,31 @@
 import { Link } from 'react-router-dom';
+import TitleBanner from "../components/TitleBanner";
 
 function Careers() {
 
   return (
-    <div>
-      <div>Careers Page</div>
+    <div> 
+      <TitleBanner>Careers</TitleBanner>
 
-      <div>
-        <Link to="/">Go To Home</Link>
-      </div>   
+      <div className="flex flex-col justify-center items-center text-center md:px-10 lg:px-30 xl:px-100 py-10 gap-2">
+        <div className="font-bold text-[25px]">Internships</div>
+        <div className="font-light">
+          At YourBookTeam, interns don’t just observe — they contribute. 
+          This is your chance to gain hands-on experience, build your portfolio, 
+          and be part of a creative, collaborative team.
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-center items-center">
+        <div className="font-bold text-[20px]">Our Interns!</div>
+
+        <div className="entire-intern">
+          <div className="picture-section">
+            
+          </div>
+        </div>
+
+      </div>
 
     </div>
   );
