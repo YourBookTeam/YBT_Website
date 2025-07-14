2dcd78969de8d1bed3f30db2e214f282cfcdc99b
Safiya-Yourbookteam-website-intern <safiyayourbookteam@gmail.com>
2025-07-14 14:05:54 -0400

    Added top level skeletal frame

--- ybt_website/src/pages/Position.jsx
+++ ybt_website/src/pages/Position.jsx
@@ -1,14 +1,25 @@
 import { Link } from 'react-router-dom';
+import Nav from "../components/Navbar"
+import Buttons from "../components/Buttons";
 
 function Position() {
 
   return (
     <div>
+      <Nav></Nav>
       <div>Position Page</div>
 
-      <div>
-        <Link to="/">Go To Home</Link>
-      </div>   
+      <section className="w-full bg[#] ">
+        <Buttons to="/careers" className="cursor-pointer ">Back to All Jobs</Buttons>
+      </section>
+      <section className='position_hero'></section>
+      <section className='open_position'>
+        <section className='apply_section'>
+          <h1 id='' className='position_Name'></h1>
+          <Buttons className={"justify-center font-bold text-sm bg-[#011829]"}>Apply</Buttons>
+        </section>
+      </section>
+
 
     </div>
   );
