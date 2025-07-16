114d02a1119c60ca2b70efd249abc1fa8db4e3a2
Nathan Samuel <nathan413591@gmail.com>
2025-07-16 12:39:01 -0500

    Updated ReadMore layout and cleaned up Services page

--- ybt_website/src/pages/Faq.jsx
+++ ybt_website/src/pages/Faq.jsx
@@ -3,7 +3,7 @@ import Dropdown1 from '../components/Dropdown1';
 
 function Faq() {
   return (
-    <div className="p-10 bg-gray-50 min-h-screen font-montserrat">
+    <div className="p-10 bg-gray-50 min-h-screen">
       <Dropdown1
         question="What is book marketing?"
         answer="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at lorem ac nisi bibendum sodales. Praesent congue velit nec orci feugiat, et convallis metus fermentum."
