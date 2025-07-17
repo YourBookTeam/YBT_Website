6a0924783eff6a53188505bd668b42bb33f75074
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2025-07-17 12:39:34 -0500

    Create SuccessStories

---
+++ ybt_website/src/components/home-page/StoryPreview.jsx
@@ -0,0 +1,33 @@
+import { FaArrowRight } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
+function StoryPreview({
+  title,
+  author,
+  children,
+  coverImgSrc,
+  flexReverse = false,
+}) {
+  const className = "flex gap-[10%] justify-center px-[15%] py-15";
+  return (
+    <div
+      className={
+        flexReverse
+          ? className + " " + "flex-row-reverse bg-off-white"
+          : className
+      }
+    >
+      <img src={coverImgSrc} alt={title + " " + "Book Cover"} className="" />
+      <div className="flex flex-col items-center gap-5">
+        <div className="text-3xl font-semibold">{title}</div>
+        <div className="text-2xl font-light">{author}</div>
+        <div>{children}</div>
+        <Link className="flex items-center self-start gap-2 bg-saffron px-7 py-2 rounded-lg font-semibold">
+          Read more
+          <FaArrowRight />
+        </Link>
+      </div>
+    </div>
+  );
+}
+export default StoryPreview;
