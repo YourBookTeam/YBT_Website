e558379d2fdf211e674da9fa2970984c692193b8
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2025-07-18 11:57:58 -0500

    Make shadow more subtle & add shadow to theme

--- ybt_website/src/components/home-page/HeroBanner.jsx
+++ ybt_website/src/components/home-page/HeroBanner.jsx
@@ -27,22 +27,16 @@ function HeroBanner() {
         <HeroSquiggle className="absolute h-full w-full"></HeroSquiggle>
         <div className="grid grid-cols-2 grid-rows-3 z-10 my-30 w-full text-3xl font-semibold">
           <div className="row-1 col-2 bg-hero-red flex flex-col justify-center p-6 pb-3 pl-3 gap-3 rounded-tl-[3rem] rounded-br-[3rem] shadow-[inset_-25px_15px_4px_rgba(0,0,0,0.25)] overflow-clip">
-            <SvgPencil className="fill-saffron place-self-end h-20 drop-shadow-[-25px_15px_2px_rgba(0,0,0,0.25)]" />
-            <div className="drop-shadow-[-25px_15px_2px_rgba(0,0,0,0.25)]">
-              WRITING
-            </div>
+            <SvgPencil className="fill-saffron place-self-end h-20 drop-shadow-hero" />
+            <div className="drop-shadow-hero">WRITING</div>
           </div>
           <div className="row-2 col-1 bg-hero-purple flex flex-col justify-center p-6 pr-9 gap-3 rounded-tl-[3rem] rounded-bl-[3rem] shadow-[inset_-25px_0px_4px_rgba(0,0,0,0.25)] overflow-clip">
-            <SvgBook className="fill-saffron place-self-end h-20 drop-shadow-[-25px_15px_2px_rgba(0,0,0,0.25)]" />
-            <div className="drop-shadow-[-25px_15px_2px_rgba(0,0,0,0.25)]">
-              REFINING
-            </div>
+            <SvgBook className="fill-saffron place-self-end h-20 drop-shadow-hero" />
+            <div className="drop-shadow-hero">REFINING</div>
           </div>
           <div className="row-3 col-2 bg-hero-green flex flex-col justify-center p-6 pt-3 pl-3 gap-3 rounded-br-[3rem] shadow-[inset_-25px_-15px_4px_rgba(0,0,0,0.25)] overflow-clip">
-            <SvgMegaphone className="fill-saffron place-self-end h-20 drop-shadow-[-25px_15px_2px_rgba(0,0,0,0.25)]" />
-            <div className="drop-shadow-[-25px_15px_2px_rgba(0,0,0,0.25)]">
-              MARKETING
-            </div>
+            <SvgMegaphone className="fill-saffron place-self-end h-20 drop-shadow-hero" />
+            <div className="drop-shadow-hero">MARKETING</div>
           </div>
         </div>
       </div>
