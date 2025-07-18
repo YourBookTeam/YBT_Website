a507a60e57eb198a86feb2f1c1236cc582853a81
britneyhu <britneyhu2@gmail.com>
2025-07-18 09:18:46 -0700

    saving work on making intern display dynamic

---
+++ ybt_website/src/data/interns.js
@@ -0,0 +1,32 @@
+import headshot_rachel from '../assets/headshots/headshot_intern_rachel.png';
+import headshot_nadun from '../assets/headshots/headshot_intern_nadun.png';
+import headshot_genevieve from '../assets/headshots/headshot_intern_genevieve.png';
+
+export const interns = [
+  {
+    id: 1,
+    name: "Rachel",
+    quote: "I was lucky to get hired by YourBookTeam as an intern, and now I’m thrilled to be a part of the paid team. My internship was an amazing journey where I got to dive into the world of editing and content creation. The team was incredibly supportive and made learning both fun and rewarding. From day one, I felt like a valued member of the group, and the experience helped me grow so much professionally. If you’re looking for an internship that offers great hands-on experience and a friendly work environment, I highly recommend YourBookTeam!",
+    headshot: headshot_rachel,
+
+    status: "display",
+  },
+
+  {
+    id: 2,
+    name: "Nadun",
+    quote: "Interning as an editor at YourBookTeam was a fantastic experience that provided both professional and personal growth. The collaborative environment and exposure to various print projects helped me develop essential editing skills. The support and mentorship from the team were invaluable, and I am deeply grateful for the experience. I recommend this internship to anyone who values hands-on learning and a chance to work on real-world projects.",
+    headshot: headshot_nadun,
+
+    status: "display",
+  },
+
+  {
+    id: 3,
+    name: "Genevieve",
+    quote: "At YourBookTeam, I collaborated with seasoned professionals who guided me through the manuscript process, enhancing my learning and skills. Working directly with the author, even remotely, deepened my understanding of publishing. I highly recommend this internship for its impact on my professional growth. If given the chance, I would choose it again. Overall, my time at YourBookTeam was positive and rewarding.",
+    headshot: headshot_genevieve,
+
+    status: "display",
+  },
+]
