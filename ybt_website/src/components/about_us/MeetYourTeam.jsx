f0c3205286ede5992adfa7c024f3208691e284c0
britneyhu <britneyhu2@gmail.com>
2025-07-21 10:52:18 -0700

    made main team dynamic with new team.js file and refactored main team into separate component

--- ybt_website/src/components/about_us/MeetYourTeam.jsx
+++ ybt_website/src/components/about_us/MeetYourTeam.jsx
@@ -1,7 +1,24 @@
-function MeetYourTeam({  }) {
+function MeetYourTeam({ main_team }) {
   return (
-    <div className="flex w-screen justify-center bg-soft-saffron py-4 font-bold text-2xl">
-      {children}
+    <div className="md:px-10 lg:px-40 xl:px-200 py-20 flex flex-col gap-30">
+      {main_team.length == 0 ? (
+            <div className="text-gray text-xl">No Team Members To Show At The Moment</div>
+        ) :
+        main_team.map((person) => (
+          <div className="flex items-center gap-10">
+            <div className="w-50 h-50 shrink-0 overflow-hidden rounded">
+              <img src={person.headshot} className="w-full h-full object-cover"/>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <div>
+                <div className="font-bold text-[25px]">{person.name}</div>
+                <div className="text-[20px]">{person.title}</div>
+              </div>
+              <div>{person.quote}</div>
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
