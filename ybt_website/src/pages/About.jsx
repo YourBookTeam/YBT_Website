f0248aea531cbd9f8d643d3f02c3704b62ef56b9
britneyhu <britneyhu2@gmail.com>
2025-08-04 09:19:23 -0700

    remove intro banner and remove incorrect team members

--- ybt_website/src/pages/About.jsx
+++ ybt_website/src/pages/About.jsx
@@ -1,5 +1,4 @@
 import TitleBanner from "../components/TitleBanner";
-import IntroBanner from "../components/about_us/IntroBanner";
 import Story from "../components/about_us/Story";
 import Values from "../components/about_us/Values";
 import MeetYourTeam from "../components/about_us/MeetYourTeam";
@@ -8,12 +7,10 @@ import { team } from "../data/team.js";
 
 function About() {
   const main_team = team.filter((person)=> person.status == "main_display");
-  const carousel_team = team.filter((person) => person.status == "carousel.display");
 
   return (
     <div>
       <TitleBanner>About Us</TitleBanner>
-      <IntroBanner/>
 
       <div className="md:px-10 lg:px-30 xl:px-100 py-10 flex flex-col gap-10">
           <Story/>
