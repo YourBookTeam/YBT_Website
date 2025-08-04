6f5cf7b463a5c06679220bc764fa62cb8b8008e0
britneyhu <britneyhu2@gmail.com>
2025-08-04 09:19:23 -0700

    remove intro banner and remove incorrect team members

--- ybt_website/src/data/team.js
+++ ybt_website/src/data/team.js
@@ -1,14 +1,11 @@
 import headshot_john from '../assets/headshots/headshot_john.png';
-import headshot_betty from '../assets/headshots/headshot_betty.png';
-import headshot_issac from '../assets/headshots/headshot_issac.png';
 import headshot_nermina from '../assets/headshots/headshot_nermina.png';
 import headshot_arfin from '../assets/headshots/headshot_arfin.png';
-import headshot_brian from '../assets/headshots/headshot_brian.png';
 
 export const team = [
   {
     id: 1,
-    name: "John",
+    name: "John Kiss",
     title: "CEO",
     quote: "An award-winning artist and peace activist, known for using her art to inspire social justice and LGBTQ+ rights. She is a grandchild of Jewish Holocaust survivors and draws inspiration from stories of survival and hope.",
     headshot: headshot_john,
@@ -18,51 +15,51 @@ export const team = [
 
   {
     id: 2,
-    name: "Betty",
-    title: "CEO",
-    quote: "With over 25 years in the publishing industry, Betty has a passion for discovering new voices and supporting both established and emerging authors. Betty is committed to shaping the future of publishing through a blend of tradition, creativity, and forward-thinking.",
-    headshot: headshot_betty,
+    name: "Nermina",
+    title: "CPO",
+    quote: "",
+    headshot: headshot_nermina,
 
     status: "main_display",
   },
 
   {
     id: 3,
-    name: "Issac",
-    title: "CFO",
-    quote: "With a strong background in finance and over 30 years of experience in the publishing industry, Isaac plays a pivotal role in driving the company’s growth and ensuring financial stability. Isaac’s expertise helps guide the company toward continued success in a rapidly evolving market.",
-    headshot: headshot_issac,
+    name: "Arfin",
+    title: "CGO",
+    quote: "",
+    headshot: headshot_arfin,
 
     status: "main_display",
   },
 
   {
     id: 4,
-    name: "Nermina",
-    title: "CPO",
+    name: "Stacy",
+    title: "COO",
     quote: "",
-    headshot: headshot_nermina,
+    headshot: "",
 
-    status: "carousel_display",
+    status: "main_display",
   },
 
   {
     id: 5,
-    name: "Arfin",
-    title: "CGO",
+    name: "Saige",
+    title: "CMO",
     quote: "",
-    headshot: headshot_arfin,
+    headshot: "",
 
-    status: "carousel_display",
+    status: "main_display",
   },
 
   {
     id: 6,
-    name: "brian",
-    title: "CTO",
+    name: "Mark",
+    title: "CDO",
     quote: "",
-    headshot: headshot_brian,
+    headshot: "",
 
-    status: "carousel_display",
+    status: "main_display",
   },
 ]
