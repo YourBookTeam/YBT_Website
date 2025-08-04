265f97fc12b11d79a2d6b4ef34224964232a82df
britneyhu <britneyhu2@gmail.com>
2025-08-04 11:05:42 -0700

    refactored team_member out of meet_your_team, fixed layout, added alt images

--- ybt_website/src/components/about_us/About_Us.jsx
+++ ybt_website/src/components/about_us/About_Us.jsx
@@ -2,7 +2,7 @@ import { FaPeopleGroup } from "react-icons/fa6";
 
 function About_Us() {
   return (
-    <div className="flex items-center justify-center gap-5">
+    <div className="flex items-center justify-center gap-5 lg:px-70">
         <div className="flex flex-col gap-10 indent-4">
             <div className="leading-loose">
                     YourBookTeam helps you turn your ideas into powerful books, even if you’re short on time or experience. From writing to editing, publishing, and marketing, our expert team guides you every step of the way.
@@ -11,7 +11,7 @@ function About_Us() {
                     We turn aspiring writers into bestselling authors by focusing on each story’s heart—turning a shaky first draft into a story that outlives you—we build legacy.
             </div>
         </div>
-        <FaPeopleGroup className="shrink-0 text-gold sm:size-60 md:size-20"/>
+        <FaPeopleGroup className="shrink-0 text-gold sm:size-60 md:size-20 lg:size-50"/>
     </div>
   );
 }
--- ybt_website/src/components/about_us/Story.jsx
+++ ybt_website/src/components/about_us/Story.jsx
@@ -2,8 +2,8 @@ import { FaBook } from "react-icons/fa";
 
 function Story() {
   return (
-    <div className="flex items-center justify-center gap-5">
-      <FaBook className="shrink-0 text-gold sm:size-60 md:size-20"/>
+    <div className="flex items-center justify-center gap-5 lg:px-70">
+      <FaBook className="shrink-0 text-gold sm:size-60 md:size-20 lg:size-50"/>
       <div className="flex flex-col gap-10 indent-4">
           <div className="leading-loose">
                   Founded by publishing expert John Kiss, YourBookTeam began with a passion for helping people share meaningful stories. We started 
