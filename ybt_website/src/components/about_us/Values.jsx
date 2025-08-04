407adaba21835e09968873f2c339de0aeb027ddf
britneyhu <britneyhu2@gmail.com>
2025-08-04 09:34:37 -0700

    made changes to values.jsx

--- ybt_website/src/components/about_us/Values.jsx
+++ ybt_website/src/components/about_us/Values.jsx
@@ -3,30 +3,30 @@ import { FaHeart, FaCrown } from "react-icons/fa";
 
 function Values() {
   return (
-    <div className="flex flex-col items-center font-bold text-[20px] gap-10 pt-20">
-        <div>Our Core Values</div>
+    <div className="flex flex-col items-center font-bold gap-10 pt-20">
+        <div className="text-4xl">Our Core Values</div>
         <div className="grid grid-cols-3 gap-50">
             <div className="flex flex-col items-center">
-            <FaHandshakeAngle size={100} className="shrink-0 text-saffron"/>
+            <FaHandshakeAngle size={100} className="shrink-0 text-gold"/>
             <div>Collaboration</div>
             </div>
             <div className="flex flex-col items-center">
-            <FaScaleBalanced size={100} className="shrink-0 text-saffron"/>
+            <FaScaleBalanced size={100} className="shrink-0 text-gold"/>
             <div>Integrity</div>
             </div>
             <div className="flex flex-col items-center">
-            <FaUsersViewfinder size={100} className="shrink-0 text-saffron"/>
+            <FaUsersViewfinder size={100} className="shrink-0 text-gold"/>
             <div className="text-center">Customer Centric</div>
             </div>
         </div>
 
         <div className="grid grid-cols-2 gap-50">
             <div className="flex flex-col items-center">
-            <FaHeart size={100} className="shrink-0 text-saffron"/>
+            <FaHeart size={100} className="shrink-0 text-gold"/>
             <div>Heart</div>
             </div>
             <div className="flex flex-col items-center">
-            <FaCrown size={100} className="shrink-0 text-saffron"/>
+            <FaCrown size={100} className="shrink-0 text-gold"/>
             <div>Legacy</div>
             </div>
         </div>
