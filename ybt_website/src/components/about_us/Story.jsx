4b403dbe19ca9d3fde51e129d64ac23a54d5516b
britneyhu <britneyhu2@gmail.com>
2025-08-12 09:58:55 -0700

    added gold background to headshots, made core values larger, adjusted spacings

--- ybt_website/src/components/about_us/Story.jsx
+++ ybt_website/src/components/about_us/Story.jsx
@@ -2,9 +2,9 @@ import { FaBook } from "react-icons/fa";
 
 function Story() {
   return (
-    <div className="flex items-center justify-center gap-5 lg:px-70">
+    <div className="flex items-center justify-center gap-10 lg:px-100 lg:pt-10">
       <FaBook className="shrink-0 text-gold lg:size-50"/>
-      <div className="flex flex-col gap-10 indent-4">
+      <div className="flex flex-col gap-10">
           <div className="leading-loose">
                   Founded by publishing expert John Kiss, YourBookTeam began with a passion for helping people share meaningful stories. We started 
                   with one goal: help people tell meaningful stories that make an impact. Our client-centric approach ensures your voice is honored 
