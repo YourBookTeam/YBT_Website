59990bd887a2229f9fe0a7cb1fc7e93e926c9aa3
britneyhu <britneyhu2@gmail.com>
2025-08-12 10:34:00 -0700

    changed team members to be dynamically mapped from team.js

--- ybt_website/src/pages/Home.jsx
+++ ybt_website/src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import SuccessStories from "../components/home-page/SuccessStories";
-import NavBar from "../components/Navbar";
 import HeroBanner from "../components/home-page/HeroBanner";
 import TeamPreview from "../components/home-page/TeamPreview";
+import { team } from "../data/team.js";
 
 function Home() {
+  const main_team = team.filter((person)=> person.status == "main_display");
+
   return (
     <>
       <HeroBanner></HeroBanner>
       <SuccessStories />
-      <TeamPreview />
+      <TeamPreview team={ main_team } />
     </>
   );
 }
