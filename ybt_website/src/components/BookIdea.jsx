18c6a0b270754241aedc4140bfd299ef8101435a
britneyhu <britneyhu2@gmail.com>
2025-08-26 10:24:23 -0700

    added back bg-gold to typewriter

--- ybt_website/src/components/BookIdea.jsx
+++ ybt_website/src/components/BookIdea.jsx
@@ -3,7 +3,7 @@ import typewriter from "../assets/typewriter.png";
 
 const BookIdea = () => {
   return (
-    <div className="w-full flex flex-col items-center justify-center py-12 px-2 relative overflow-hidden bg-center bg-size-[700px] bg-no-repeat bg-blend-soft-light" style={{ backgroundImage: `url(${typewriter})` }}>
+    <div className="w-full flex flex-col items-center justify-center py-12 px-2 relative overflow-hidden bg-gold bg-center bg-size-[700px] bg-no-repeat bg-blend-soft-light" style={{ backgroundImage: `url(${typewriter})` }}>
       <h2 className="text-white text-4xl md:text-5xl font-bold text-center drop-shadow-hero mb-8">
         Have a book idea or want to learn more?
       </h2>
