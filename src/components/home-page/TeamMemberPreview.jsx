3e4abe37a3584d05e7a6433d11e25676ce8ba2e0
britneyhu <britneyhu2@gmail.com>
2025-09-09 10:55:13 -0700

    changed headshots to links and added hover effect to them

--- src/components/home-page/TeamMemberPreview.jsx
+++ src/components/home-page/TeamMemberPreview.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
+
 function TeamMemberPreview({ person }) {
+  const scrollToTop = () => {
+      window.scrollTo(0,0);
+  }
+
   return (
-    <div className="flex flex-col items-center gap-5">
-      <img src={person.headshot} className="rounded-full h-50 w-50 object-cover bg-gold"/>
-      <div className="text-2xl font-semibold">{person.name}</div>
-      <div className="text-xl">{person.position}</div>
-    </div>
+    <Link to="/about#meet-us" onClick={scrollToTop} className="flex flex-col items-center gap-5">
+        <img src={person.headshot} className="rounded-full h-50 w-50 object-cover bg-gold hover:opacity-80 transition-colors duration-200"/>
+        <div className="text-2xl font-semibold">{person.name}</div>
+        <div className="text-xl">{person.position}</div>
+    </Link>
+    
   );
 }
 export default TeamMemberPreview;
