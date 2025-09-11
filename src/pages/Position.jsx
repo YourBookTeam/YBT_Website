2755f2cfa2bc66c6d6f6a05b3c9a328672e14997
Safiya-Yourbookteam-website-intern <safiyayourbookteam@gmail.com>
2025-09-11 14:29:45 -0400

    change the import of the positions array from the positions.js file due the previous way breaking the page. Removed the s from the button component and updated the map function to load in the array items for the responsibilites, qualifications and benefts in an ordered list.

--- src/pages/Position.jsx
+++ src/pages/Position.jsx
@@ -1,9 +1,8 @@
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import Buttons from "../components/Buttons";
-import heroImg from '../assets/Join-The-Team.jpg'
-import jobData from "../data/positions";
+import Button from "../components/Button";
+import { positions } from '../data/positions';
 
 
 function Position() {
@@ -12,7 +11,7 @@ function Position() {
     const [position, setPosition] = useState(null);
 
     useEffect(() => {
-      const selectedPosition = jobData.find(position => position.id === parseInt(id));
+      const selectedPosition = positions.find(position => position.id === parseInt(id));
         if (selectedPosition){
         setPosition(selectedPosition)
       }else{
@@ -24,59 +23,56 @@ function Position() {
   
   return (
     <div>
-       <section className="w-full h-8 my-6 p-1 bg-[#FFE7A6] ">
-        <Buttons to="/careers" className="cursor-pointer underline">Back to All Jobs</Buttons>
-        </section>
-        <section className="position_hero h-50 mb-13 flex justify-center">
-          <img src={heroImg} alt="This is the position page's hero banner. Four individuals high" className="h-50 w-100"/>
+       <section className="w-full h-8 my-3 mb-15 p-1">
+        <Button to="/careers" className="cursor-pointer underline">Back to All Jobs</Button>
         </section>
          <section className='open_position flex justify-center w-full'>
           <div className='title_containter sm:w-150 md:w-190 lg:w-240'>
             <section className='position_title flex justify-between sm:w-150 md:w-190 lg:w-240 m-1'>
               <h1 className='position_name sm:text-md md:text-2xl lg:text-2xl font-semibold content-end'>{position.title}</h1>
-              <Buttons className={"cursor-pointer justify-center font-bold text-sm text-white bg-[#011829] px-5 py-2 w-30 rounded"}>Apply</Buttons>
+              <Button className={"cursor-pointer justify-center font-bold text-sm text-white bg-[#011829] px-5 py-2 w-30 rounded"}>Apply</Button>
             </section>
-            <hr className=" border border-black border-solid"/>
+            <hr className=" border border-black border-solid my-5"/>
           </div>
         </section>
         <section className='positon_details px-17 my-3.5 text-left'>
            <section className='position_Description'>
-          <h2 className="font-bold ">Description:</h2>
-           <p>{position.description}</p>
-          <h2 className="font-bold inline-block">Location:</h2><p className='inline-block ml-1.5'>{position.location}</p><br />
-          <h2 className="font-bold inline-block">Duration:</h2><p className='inline-block ml-1.5'>{position.duration}</p><br />
+          <h2 className="font-bold mb-2">Description:</h2>
+           <p className='mb-3 text-justify leading-relaxed'>{position.description}</p>
+          <h2 className="font-bold inline-block mb-3">Location:</h2><p className='inline-block ml-1.5'>{position.location}</p><br />
+          <h2 className="font-bold inline-block mb-3">Duration:</h2><p className='inline-block ml-1.5'>{position.duration}</p><br />
           <h2 className="font-bold inline-block">Compensation:</h2><p className='inline-block ml-1.5'>{position.compensation}</p><br />
         </section>
         <section className='position_responsibility my-10'>
-          <h2 className="font-bold ">Key Responsibilities:</h2>
-           <ul>
-            {position.responsibilities.map((item, index) => (
-              <li key={item.id || index}>{item.value}</li>
+          <h2 className="font-bold mb-2">Key Responsibilities:</h2>
+           <ol className="list-decimal ml-5">
+              {(position?.responsibilities || []).map((item, index) => (
+              <li key={item.id || index} className="my-4">{item.value || item}</li>
             ))}
-          </ul>
+          </ol>
         </section>
         <section className='position_qualification my-10'>
-          <h2 className="font-bold ">Qualifications:</h2>
-           <ul>
-            {position.qualifications.map((item, index) => (
-              <li key={item.id || index}>{item.value}</li>
+          <h2 className="font-bold mb-2">Qualifications:</h2>
+           <ol className="list-decimal ml-5">
+              {(position?.qualifications || []).map((item, index) => (
+              <li key={item.id || index} className="my-4">{item.value || item}</li>
             ))}
-          </ul>
+          </ol>
         </section>
         <section className='position_Benefits my-10'>
-          <h2 className="font-bold ">Benefits:</h2>
-          <ul>
-            {position.benefits.map((item, index) => (
-              <li key={item.id || index}>{item.value}</li>
+          <h2 className="font-bold  mb-2">Benefits:</h2>
+          <ol className="list-decimal ml-5">
+              {(position?.benefits || []).map((item, index) => (
+              <li key={item.id || index} className="my-4">{item.value || item}</li>
             ))}
-          </ul>
+          </ol>
         </section>
         </section>
-          <section className='apply_btn flex justify-center m-1'>
-            <Buttons className={"cursor-pointer justify-center font-bold text-sm text-white bg-[#011829] px-5 py-2 w-60 rounded"}>Apply</Buttons>
+          <section className='apply_btn flex justify-center py-6'>
+            <Button className={"cursor-pointer justify-center font-bold text-sm text-white bg-[#011829] px-5 py-2 w-60 rounded"}>Apply</Button>
           </section>
-          <section className="w-full h-8 mt-10 p-1 bg-[#FFE7A6] text-center">
-            <Buttons to="/careers" className="cursor-pointer underline">Back to All Jobs</Buttons>
+          <section className="w-full h-8 mt-10 p-1 text-center">
+            <Button to="/careers" className="cursor-pointer underline">Back to All Jobs</Button>
           </section>
     </div>
   );
