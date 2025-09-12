13bb4c9cc565267524b2005d6067e4e75c94e77d
Safiya-Yourbookteam-website-intern <safiyayourbookteam@gmail.com>
2025-09-12 14:20:05 -0400

    Sized for responsiveness on all screens

--- src/pages/Position.jsx
+++ src/pages/Position.jsx
@@ -28,51 +28,52 @@ function Position() {
         </section>
          <section className='open_position flex-col justify-items-center w-full'>
           <img src={position.image} alt='' className='w-100 my-10 pb-5'></img>
-          <div className='title_containter sm:w-150 md:w-190 lg:w-240 pb-10'>
-            <section className='position_title flex justify-between sm:w-150 md:w-190 lg:w-240 m-1'>
-              <h1 className='position_name sm:text-md md:text-2xl lg:text-2xl font-semibold content-end'>{position.title}</h1>
+          <div className='title_containter w-80 md:w-170 lg:w-240 pb-10'>
+            <section className="w-full flex flex-col items-center md:flex-row md:justify-between md:w-170 md:px-15 lg:w-240 m-1">
+
+              <h1 className='position_name text-md mb-5 md:text-2xl lg:text-2xl font-semibold content-end'>{position.title}</h1>
               <Button to="https://books.click/internship" className={"cursor-pointer justify-center font-bold text-sm text-white bg-[#011829] px-5 py-2 w-30 rounded"}>Apply</Button>
             </section>
             <hr className=" border border-black border-solid my-5"/>
           </div>
         </section>
-        <section className='positon_details px-17 my-3.5 text-left'>
-           <section className='position_Description'>
-          <h2 className="font-bold mb-2">Description:</h2>
-           <p className='mb-3 text-justify leading-relaxed'>{position.description}</p>
-          <h2 className="font-bold inline-block mb-3">Location:</h2><p className='inline-block ml-1.5'>{position.location}</p><br />
-          <h2 className="font-bold inline-block mb-3">Duration:</h2><p className='inline-block ml-1.5'>{position.duration}</p><br />
-          <h2 className="font-bold inline-block">Compensation:</h2><p className='inline-block ml-1.5'>{position.compensation}</p><br />
-        </section>
-        <section className='position_responsibility my-10'>
-          <h2 className="font-bold mb-2">Key Responsibilities:</h2>
-           <ol className="list-decimal ml-5">
+        <section className='positon_details flex-col justify-items-center px-17 my-3.5 text-left'>
+           <section className='position_Description w-80 md:w-170 lg:w-240'>
+            <h2 className="font-bold mb-2">Description:</h2>
+            <p className='mb-3 text-justify leading-relaxed'>{position.description}</p>
+            <h2 className="font-bold inline-block mb-3">Location:</h2><p className='inline-block ml-1.5'>{position.location}</p><br />
+            <h2 className="font-bold inline-block mb-3">Duration:</h2><p className='inline-block ml-1.5'>{position.duration}</p><br />
+            <h2 className="font-bold inline-block">Compensation:</h2><p className='inline-block ml-1.5'>{position.compensation}</p><br />
+          </section>
+          <section className='position_responsibility my-10 w-80 md:w-170 lg:w-240'>
+            <h2 className="font-bold mb-2">Key Responsibilities:</h2>
+            <ol className="list-decimal ml-5">
               {(position?.responsibilities || []).map((item, index) => (
-              <li key={item.id || index} className="my-4">{item.value || item}</li>
-            ))}
-          </ol>
-        </section>
-        <section className='position_qualification my-10'>
-          <h2 className="font-bold mb-2">Qualifications:</h2>
-           <ol className="list-decimal ml-5">
-              {(position?.qualifications || []).map((item, index) => (
-              <li key={item.id || index} className="my-4">{item.value || item}</li>
-            ))}
-          </ol>
-        </section>
-        <section className='position_Benefits my-10'>
-          <h2 className="font-bold  mb-2">Benefits:</h2>
-          <ol className="list-decimal ml-5">
-              {(position?.benefits || []).map((item, index) => (
-              <li key={item.id || index} className="my-4">{item.value || item}</li>
-            ))}
-          </ol>
-        </section>
+                <li key={item.id || index} className="my-4">{item.value || item}</li>
+              ))}
+            </ol>
+          </section>
+          <section className='position_qualification my-10 w-80 md:w-170 lg:w-240'>
+            <h2 className="font-bold mb-2">Qualifications:</h2>
+            <ol className="list-decimal ml-5">
+                {(position?.qualifications || []).map((item, index) => (
+                  <li key={item.id || index} className="my-4">{item.value || item}</li>
+                ))}
+            </ol>
+          </section>
+          <section className='position_Benefits my-10 w-80 md:w-170 lg:w-240'>
+            <h2 className="font-bold  mb-2">Benefits:</h2>
+            <ol className="list-decimal ml-5">
+                {(position?.benefits || []).map((item, index) => (
+                  <li key={item.id || index} className="my-4">{item.value || item}</li>
+                ))}
+            </ol>
+          </section>
         </section>
           <section className='apply_btn flex justify-center py-6'>
             <Button to="https://books.click/internship" className={"cursor-pointer justify-center font-bold text-sm text-white bg-[#011829] px-5 py-2 w-60 rounded"}>Apply</Button>
           </section>
-          <section className="w-full h-8 mt-10 p-1 text-center">
+          <section className="w-full h-8 mt-10 text-center">
             <Button to="/careers" className="cursor-pointer underline">Back to All Jobs</Button>
           </section>
     </div>
