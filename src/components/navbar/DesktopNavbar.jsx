4b9c8f3e8ff3bfd474def2554e446a28c51fae09
Safiya-Yourbookteam-website-intern <safiyayourbookteam@gmail.com>
2025-09-18 12:56:47 -0400

    Added career nav item to nav

--- src/components/navbar/DesktopNavbar.jsx
+++ src/components/navbar/DesktopNavbar.jsx
@@ -4,17 +4,43 @@ import DiscoverButton from "./DiscoveryButton";
 
 function DesktopNavbar() {
   return (
-    <nav className="hidden md:flex px-12 py-5 flex justify-between items-center w-screen bg-white">
+<nav className="hidden md:flex px-12 py-5 flex justify-between items-center w-screen bg-white">
       <div className="pr-12">
         <Logo className="fill-gold"></Logo>
       </div>
 
         <div className="flex gap-16 lg:gap-28">
-            <Link to="/about">About</Link>
-            <Link to="/services">Services</Link>
-            <Link to="/#success-stories">Success Stories</Link>
-            <Link to="/faq">FAQ</Link>
-            <Link to="/careers">Careers</Link>
+
+            <Link to="/about" className="relative group">
+                <span className="relative pb-1">
+                  <span className="inline-block transition-transform duration-200 ease-out group-hover:scale-105">About</span>
+                  <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
+                </span>
+            </Link>
+            <Link to="/services" className="relative group">
+                <span className="relative pb-1">
+                  <span className="inline-block transition-transform duration-200 ease-out group-hover:scale-105">Services</span>
+                  <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
+                </span>
+            </Link>
+            <Link to="/#success-stories" className="relative group whitespace-nowrap">
+                <span className="relative pb-1">
+                  <span className="inline-block transition-transform duration-200 ease-out group-hover:scale-105">Success Stories</span>
+                  <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
+                </span>
+            </Link>
+            <Link to="/faq" className="relative group">
+                <span className="relative pb-1">
+                  <span className="inline-block transition-transform duration-200 ease-out group-hover:scale-105">FAQ</span>
+                  <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
+                </span>
+            </Link>
+            <Link to="/careers" className="relative group">
+                <span className="relative pb-1">
+                  <span className="inline-block transition-transform duration-200 ease-out group-hover:scale-105">Careers</span>
+                  <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
+                </span>
+            </Link>
         </div>
 
       <DiscoverButton/>
