01e4dde95e86ac44f6b22fb3031d05a62acbd478
britneyhu <britneyhu2@gmail.com>
2025-09-24 10:19:40 -0700

    changed file names to internships

---
+++ src/pages/Internships.jsx
@@ -0,0 +1,112 @@
+import { useState } from "react";
+import { useRef } from "react";
+import { interns } from "../data/interns.js";
+import { positions } from "../data/positions.js";
+import TitleBanner from "../components/TitleBanner.jsx";
+import Interns from "../components/internships_page/Interns.jsx";
+import SearchBar from "../components/internships_page/SearchBar.jsx";
+import Pagination from "../components/internships_page/Pagination.jsx";
+import CurrentOpenings from "../components/internships_page/CurrentOpenings.jsx";
+
+function Internships() {
+  const [query, setQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const openJobs = positions.filter((job) => job.status == "open");
+  const [searchedJobs, setSearchedJobs] = useState(openJobs);
+  const sectionRef = useRef(null);
+
+  const itemsPerPage = 6;
+  const totalPages = Math.ceil(openJobs.length / itemsPerPage);
+  const startIndex = itemsPerPage * (currentPage - 1);
+  const endIndex = startIndex + itemsPerPage;
+  const displayedJobs = searchedJobs.slice(startIndex, endIndex);
+
+  const displayedInterns = interns.filter(
+    (intern) => intern.status == "display"
+  );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    query.toLowerCase();
+
+    const sorted = [...openJobs].sort((a, b) => {
+      const aText = (a.title + " " + a.description).toLowerCase();
+      const bText = (b.title + " " + b.description).toLowerCase();
+
+      const aMatch = aText.includes(query) ? 1 : 0;
+      const bMatch = bText.includes(query) ? 1 : 0;
+
+      return bMatch - aMatch;
+    });
+
+    setSearchedJobs(sorted);
+    setQuery("");
+  };
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+      sectionRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+      sectionRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const handlePageClick = (num) => {
+    setCurrentPage(num);
+    sectionRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <div>
+      <TitleBanner>Internships</TitleBanner>
+
+      <div className="flex flex-col justify-center items-center text-center px-5 md:px-10 lg:px-30 xl:px-50 2xl:px-100 pt-10 pb-20 gap-2">
+        <div className="font-light font-secondary">
+          At YourBookTeam, interns don’t just observe — they contribute. This is
+          your chance to gain hands-on experience, build your portfolio, and be
+          part of a creative, collaborative team.
+        </div>
+      </div>
+
+      <Interns displayedInterns={displayedInterns} />
+
+      <div className="flex flex-col justify-center items-center gap-10">
+        <div
+          id="current-openings"
+          className="font-bold text-4xl"
+          ref={sectionRef}
+        >
+          Current Openings
+        </div>
+        <SearchBar
+          query={query}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+        />
+      </div>
+
+      <CurrentOpenings displayedJobs={displayedJobs} />
+
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        handlePrevious={handlePrevious}
+        handleNext={handleNext}
+        handlePageClick={handlePageClick}
+      />
+    </div>
+  );
+}
+
+export default Internships;
