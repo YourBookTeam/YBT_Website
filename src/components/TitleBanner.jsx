706626a1c41e0fe77147a6e04c603f59cd0d0876
britneyhu <britneyhu2@gmail.com>
2025-09-26 09:04:11 -0700

    fixed underline not centered in mobile

--- src/components/TitleBanner.jsx
+++ src/components/TitleBanner.jsx
@@ -1,6 +1,6 @@
 function TitleBanner({ children }) {
   return (
-      <div className="text-gold text-3xl md:text-4xl xl:text-5xl text-center font-bold p-10  flex-col justify-items-center">
+      <div className="text-gold text-3xl md:text-4xl xl:text-5xl text-center font-bold p-10 flex flex-col justify-center items-center">
         {children}
         <div className="bg-gold h-1 w-40 mt-5"></div>
       </div>
