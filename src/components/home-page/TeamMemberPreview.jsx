47811b1babc75be147f131066a4047c068b4a6d2
Safiya-Yourbookteam-website-intern <safiyayourbookteam@gmail.com>
2025-10-03 13:21:52 -0400

    First, I duplicated the team list to achieve a continuous, smooth scrolling effect. Then, I altered the designs of the images from the sphere to match the example given and added the chief's abbreviated titles. Placing them in their own section below the image.

--- src/components/home-page/TeamMemberPreview.jsx
+++ src/components/home-page/TeamMemberPreview.jsx
@@ -1,17 +1,19 @@
 import { Link } from "react-router-dom";
 
-function TeamMemberPreview({ person }) {
+function TeamMemberPreview({ person, duplicate = false }) {
   const scrollToTop = () => {
-      window.scrollTo(0,0);
-  }
+    window.scrollTo(0,0);
+  };
 
   return (
-    <Link to={"/about#" + person.link} onClick={scrollToTop} className="flex flex-col items-center gap-5">
-        <img src={person.headshot} className="rounded-full h-50 w-50 object-cover bg-gold hover:opacity-80 transition-colors duration-200"/>
-        <div className="text-2xl font-semibold">{person.name}</div>
-        <div className="text-xl">{person.position}</div>
+    <Link to={"/about#" + person.link} onClick={scrollToTop} className="flex flex-col items-center gap-5" aria-hidden={duplicate ? "true" : undefined} tabIndex={duplicate ? -1 : 0}>
+      <img src={person.headshot} alt={`${person.name} headshot`} className="rounded-tr-3xl h-80 w-50 object-cover bg-gold hover:opacity-80 transition-colors duration-200"/>
+      <section className="team-info w-full flex">
+        <div className="text-base font-semibold">{person.name}</div>
+        <div className="text-base ml-3">{person.title_abbreviation}</div>
+      </section>
     </Link>
-    
   );
 }
+
 export default TeamMemberPreview;
