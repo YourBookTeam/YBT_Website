59d032a05f2a8aaba31e13a4d5ba5c579ea58e48
britneyhu <britneyhu2@gmail.com>
2025-10-09 09:31:35 -0700

    editing logo in background

--- src/components/footer/DesktopFooter.jsx
+++ src/components/footer/DesktopFooter.jsx
@@ -12,26 +12,38 @@ const DesktopFooter = () => {
   const currentPage = useLocation();
 
   return (
-    <footer className="hidden lg:flex flex-col bg-gold">
-      <div className="w-full flex justify-between 2xl:justify-center p-15">
+    <footer className="hidden lg:flex flex-col bg-gold relative">
+      <div className="w-full grid grid-cols-2">
 
-        <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-10 p-15">
           <MainLogo/>
-          <Statement className="max-w-[60%]"/>
+          <Statement className="max-w-[70%]"/>
           <Socials/>
           <BackButton/>
         </div>
 
-        <div className="relative">
-          <div className="relative z-10 flex gap-5 xl:gap-10 2xl:gap-20">
-            <QuickLinks currentPage={currentPage.pathname}/>
-            <ContactInfo/>
-          </div>
-          <div className="absolute left-0 right-[15%] top-0 bottom-0 flex items-center justify-center z-0">
-            <Logo className="fill-white size-[143%] opacity-15"/>
-          </div>
-        </div>
+        <div className="relative flex justify-center">
+            <div className="flex gap-5 p-15 xl:gap-10 2xl:gap-20">
+              <QuickLinks currentPage={currentPage.pathname}/>
+              <ContactInfo/>
+            </div>
 
+            <div 
+              className="
+                absolute z-0
+                inset-y-0 left-1/2 -translate-x-1/2
+                flex items-center
+              ">
+              <Logo 
+                preserveAspectRatio="none" 
+                className="
+                  fill-white opacity-15
+                  h-full
+                  w-auto
+                  scale-x-150
+                "/>
+            </div>
+        </div>
       </div>
 
       <Copyright/>
