6b072d53d193dfbd4b28dac255f1f15e88164e77
Safiya-Yourbookteam-website-intern <safiyayourbookteam@gmail.com>
2025-10-09 12:30:07 -0400

    made some style changes to match given visuals for updates.

--- src/components/home-page/TeamMemberPreview.jsx
+++ src/components/home-page/TeamMemberPreview.jsx
@@ -4,8 +4,8 @@ function TeamMemberPreview({ person }) {
   const scrollToTop = () => window.scrollTo(0, 0);
 
   return (
-    <Link to={"/about#" + person.link} onClick={scrollToTop} className="flex flex-col items-center gap-5 flex-shrink-0 w-60">
-      <img src={person.headshot} alt={person.name} className="rounded-tr-3xl h-90 w-60 object-cover bg-gold hover:opacity-80 transition-colors duration-200"/>
+    <Link to={"/about#" + person.link} onClick={scrollToTop} className="flex flex-col items-center gap-10 flex-shrink-0 w-67">
+      <img src={person.headshot} alt={person.name} className="rounded-tr-3xl h-90 w-60 mx-10 object-cover bg-[#F9F9F9] drop-shadow-lg hover:opacity-80 transition-colors duration-200"/>
       <section className="team-info w-full flex">
         <div className="text-base font-semibold">{person.name}</div>
         <div className="text-base ml-3">{person.title_abbreviation}</div>
