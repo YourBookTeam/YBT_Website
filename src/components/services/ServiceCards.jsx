78e95f45dfe6487071baa72d64c6983f612aaad4
Safiya-Yourbookteam-website-intern <safiyayourbookteam@gmail.com>
2025-11-10 13:05:24 -0500

    Add responsiveness for all screens.

--- src/components/services/ServiceCards.jsx
+++ src/components/services/ServiceCards.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ServiceCards({services}){
     console.log (services)
     return(
-        <div className="grid grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {services.map((item) =>(
                 <div className="bg-white shadow-sm rounded-lg p-6 transform transition-all duration-300 hover:-translate-y-2 hover:shadow-xl cursor-pointer" key={item.id}>
                     <h3 className="text-xl font-semibold text-gold mb-4">{item.title}</h3>
