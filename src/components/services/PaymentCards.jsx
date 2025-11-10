edefdaa1207efccd0757a873b47be38128b6b9f2
Safiya-Yourbookteam-website-intern <safiyayourbookteam@gmail.com>
2025-11-10 13:55:30 -0500

    The cards in these components were collapsing on each other when in screen size 1024. So I adjusted them to a grid layout that wold allow them to be displayed without doing so and then align in a single column one in mobile screen sizes.

--- src/components/services/PaymentCards.jsx
+++ src/components/services/PaymentCards.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 function paymentCards({cards}){
     return(
-        <div className="w-285 flex justify-evenly">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 px-4 sm:px-6 lg:px-12">
             {cards.map((item)=>(
-                 <div className="bg-white drop-shadow-md rounded-lg p-6 w-85 h-120" key={item.id}>
+                 <div className="bg-white drop-shadow-md rounded-lg p-6 w-full max-w-sm mx-auto flex flex-col" key={item.id}>
                     <h3 className="text-xl font-semibold text-gold my-3">{item.title}</h3>
-                    <ul className="space-y-1">
+                    <ul className="space-y-2 flex-1">
                         {item.details.map((detail, index)=> (
                         <li key={index} className="flex items-start space-x-3">
-                            <span className="mt-4 w-1 h-1 bg-gold rounded-full flex-shrink-0"></span>
+                            <span className="mt-1 w-2 h-2 bg-gold rounded-full flex-shrink-0"></span>
                             <p className="leading-relaxed p-1 rounded">{detail}</p>
                         </li>
 
