92b2b8b7128f5c48da50e9f57fa899a38601da8d
Onokome <faithcol25@gmail.com>
2025-11-13 20:14:25 +0100

    fix/nav_bar_adjustments

--- src/components/navbar/DesktopNavbar.jsx
+++ src/components/navbar/DesktopNavbar.jsx
@@ -6,56 +6,65 @@ function DesktopNavbar() {
   return (
     <nav className="hidden md:flex mb-4 px-2 lg:px-4 pt-5 pb-3 justify-between items-center w-screen bg-white border-b-3 border-[var(--color-light-gray)]">
       <div className="pr-6 lg:pr-12">
-        <DesktopLogo className="fill-gold"></DesktopLogo>
+        <DesktopLogo className="fill-gold md:w-32 md:h-20 lg:w-40 lg:h-24"></DesktopLogo>
       </div>
 
-      <div className="flex gap-4 lg:gap-8">
-        <Link to="/about" className="relative group">
-          <span className="relative pb-1">
-            <span className="inline-block text-sm lg:text-base transition-transform duration-200 ease-out group-hover:scale-105">
-              ABOUT
+      <div className="flex items-center gap-2 lg:gap-4">
+        <div className="flex gap-4 lg:gap-8">
+          <Link to="/" className="relative group">
+            <span className="relative pb-1">
+              <span className="inline-block text-sm lg:text-lg transition-transform duration-200 ease-out group-hover:scale-105">
+                HOME
+              </span>
+              <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
             </span>
-            <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
-          </span>
-        </Link>
-        <Link to="/pricing" className="relative group">
-          <span className="relative pb-1">
-            <span className="inline-block text-sm lg:text-base transition-transform duration-200 ease-out group-hover:scale-105">
-              PRICING
+          </Link>
+          <Link to="/about" className="relative group">
+            <span className="relative pb-1">
+              <span className="inline-block text-sm lg:text-lg transition-transform duration-200 ease-out group-hover:scale-105">
+                ABOUT
+              </span>
+              <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
             </span>
-            <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
-          </span>
-        </Link>
-        <Link
-          to="/#success-stories"
-          className="relative group whitespace-nowrap"
-        >
-          <span className="relative pb-1">
-            <span className="inline-block text-sm lg:text-base transition-transform duration-200 ease-out group-hover:scale-105">
-              SUCCESS STORIES
+          </Link>
+          <Link to="/pricing" className="relative group">
+            <span className="relative pb-1">
+              <span className="inline-block text-sm lg:text-lg transition-transform duration-200 ease-out group-hover:scale-105">
+                PRICING
+              </span>
+              <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
             </span>
-            <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
-          </span>
-        </Link>
-        <Link to="/internships" className="relative group">
-          <span className="relative pb-1">
-            <span className="inline-block text-sm lg:text-base transition-transform duration-200 ease-out group-hover:scale-105">
-              INTERNSHIPS
+          </Link>
+          <Link
+            to="/#success-stories"
+            className="relative group whitespace-nowrap"
+          >
+            <span className="relative pb-1">
+              <span className="inline-block text-sm lg:text-lg transition-transform duration-200 ease-out group-hover:scale-105">
+                SUCCESS STORIES
+              </span>
+              <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
             </span>
-            <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
-          </span>
-        </Link>
-        <Link to="/faq" className="relative group">
-          <span className="relative pb-1">
-            <span className="inline-block text-sm lg:text-base transition-transform duration-200 ease-out group-hover:scale-105">
-              FAQ
+          </Link>
+          <Link to="/internships" className="relative group">
+            <span className="relative pb-1">
+              <span className="inline-block text-sm lg:text-lg transition-transform duration-200 ease-out group-hover:scale-105">
+                INTERNSHIPS
+              </span>
+              <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
             </span>
-            <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
-          </span>
-        </Link>
+          </Link>
+          <Link to="/faq" className="relative group">
+            <span className="relative pb-1">
+              <span className="inline-block text-sm lg:text-lg transition-transform duration-200 ease-out group-hover:scale-105">
+                FAQ
+              </span>
+              <span className="absolute bottom-0 left-0 right-0 h-0.5 bg-gold origin-center scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
+            </span>
+          </Link>
+        </div>
+        <DiscoveryButton />
       </div>
-
-      <DiscoveryButton />
     </nav>
   );
 }
--- src/components/navbar/DiscoveryButton.jsx
+++ src/components/navbar/DiscoveryButton.jsx
@@ -3,7 +3,7 @@ import Button from "../Button";
 function DiscoveryButton({ onClick }) {
   return (
     <div>
-      <Button gold tall to="/contact" onClick={onClick} className="mx-5">
+      <Button gold tall to="/contact" onClick={onClick} className="mx-5 text-sm">
         Book a Free Discovery Call
       </Button>
     </div>
