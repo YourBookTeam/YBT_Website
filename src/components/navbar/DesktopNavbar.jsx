f67ac5fa0593aefb62f1b955e10635c135bd42d0
britneyhu <britneyhu2@gmail.com>
2025-11-17 10:47:50 -0800

    increased gaps of navbar

--- src/components/navbar/DesktopNavbar.jsx
+++ src/components/navbar/DesktopNavbar.jsx
@@ -9,7 +9,7 @@ function DesktopNavbar() {
         <DesktopLogo className="fill-gold -mt-2 lg:-mt-3 md:w-32 md:h-20 lg:w-40 lg:h-24 2xl:w-60 2xl:h-32"></DesktopLogo>
       </div>
 
-      <div className="flex items-center gap-4 lg:gap-8">
+      <div className="flex items-center gap-4 lg:gap-16">
         <div className="flex gap-8 lg:gap-12">
           <Link to="/" className="relative group">
             <span className="relative pb-1">
